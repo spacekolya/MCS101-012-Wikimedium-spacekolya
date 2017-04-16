80f6cd11d61f03013a0d646b459290a906149add
spacekolya <middlelebowski@gmail.com>
2017-04-16 20:40:25 +0300

    Занятие 4.2

---
+++ javascripts/sidebar.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  $('.sidebar a').each(function() {
+
+    $(this).on('click', function(e) {
+
+      e.preventDefault();
+
+      var scrollTo = $(this).attr('href');
+
+      var whitespace = scrollTo == '#s1' ? 120 : 30
+
+      $('html,body').animate({
+
+        scrollTop: $(scrollTo).offset().top - whitespace
+
+      }, 1000);
+
+    });
+
+  });
+
+});
